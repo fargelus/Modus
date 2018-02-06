506d66ff367b61c8c195627e73de8d76ce016e94
fargelus <ddraudred@gmail.com>
2018-02-06 23:09:48 +0300

    Prior nav fade

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,16 +1,33 @@
 $(() => {
-  $('.js-nav-link').each(function() {
-    const $nav_link = $(this);
-    const anchor_href = $nav_link.attr('href');
-    const $section_anchor = $(anchor_href);
-    const $root = $('html,body');
+  function scrollOnTopNavLinkClick() {
+    $('.js-nav-link').each(function() {
+      const $nav_link = $(this);
+      const anchor_href = $nav_link.attr('href');
+      const $section_anchor = $(anchor_href);
+      const $root = $('html,body');
 
-    $nav_link.click(() => {
-      $root.animate({
-        scrollTop: $section_anchor.offset().top
-      }, 600);
+      $nav_link.click(() => {
+        $root.animate({
+          scrollTop: $section_anchor.offset().top
+        }, 600);
 
-      return false;
+        return false;
+      });
     });
-  });
+  }
+
+  function mainNavVisibilityChange() {
+    const $prior_nav = $('.prior-nav');
+    $(window).scroll(function() {
+      const scrool_value = $(this).scrollTop();
+      if (scrool_value >= 200) {
+        $prior_nav.fadeIn();
+      } else {
+        $prior_nav.fadeOut();
+      }
+    });
+  }
+
+  scrollOnTopNavLinkClick();
+  mainNavVisibilityChange();
 });
