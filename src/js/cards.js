a9589e9dcfae79b80efd015666883657c4022349
fargelus <ddraudred@gmail.com>
2018-02-24 13:58:10 +0300

    Popup block update

--- src/js/cards.js
+++ src/js/cards.js
@@ -1,13 +1,24 @@
 $(() => {
   const $cards_deck_item = $('.cards-deck__item');
+
   $cards_deck_item.hover(function() {
     const $current_popup_block = $(this).children('.popup-block');
-    $current_popup_block.slideToggle(
-      { 'start': function() {
-                  if ($(this).css('display') === 'block') {
+
+    const fade_options =
+    { 'start': function() {
+                  if ($(this).is(':visible')) {
                       $(this).css('display', 'flex');
                     }
+
+                  const $popup_childrens = $(this).children();
+                  if ($popup_childrens.hasClass('smooth-up')) {
+                      $popup_childrens.removeClass('smooth-up').addClass('smooth-down');
+                  } else {
+                    $popup_childrens.removeClass('smooth-down').addClass('smooth-up');
                   }
-      });
+                },
+    };
+
+    $current_popup_block.fadeToggle(fade_options);
   });
 });
