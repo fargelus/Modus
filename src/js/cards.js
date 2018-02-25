484eb9abbc2aab08c0bc1f3d6c58a426adfd1ac7
fargelus <ddraudred@gmail.com>
2018-02-25 15:45:51 +0300

    Cards code refactor

--- src/js/cards.js
+++ src/js/cards.js
@@ -1,24 +1,30 @@
 $(() => {
-  const $cards_deck_item = $('.cards-deck__item');
+  function makePopupCard() {
+    const $cards_deck_item = $('.cards-deck__item');
 
-  $cards_deck_item.hover(function() {
-    const $current_popup_block = $(this).children('.popup-block');
+    $cards_deck_item.hover(function() {
+      const $current_popup_block = $(this).children('.popup-block');
 
-    const fade_options =
-    { 'start': function() {
-                  if ($(this).is(':visible')) {
-                      $(this).css('display', 'flex');
+      const fade_options =
+      { 'start': function() {
+                    if ($(this).is(':visible')) {
+                        $(this).css('display', 'flex');
+                      }
+
+                    const $popup_childrens = $(this).children();
+                    if ($popup_childrens.hasClass('smooth-up')) {
+                        $popup_childrens.removeClass('smooth-up')
+                        .addClass('smooth-down');
+                    } else {
+                      $popup_childrens.removeClass('smooth-down')
+                      .addClass('smooth-up');
                     }
+                  },
+      };
 
-                  const $popup_childrens = $(this).children();
-                  if ($popup_childrens.hasClass('smooth-up')) {
-                      $popup_childrens.removeClass('smooth-up').addClass('smooth-down');
-                  } else {
-                    $popup_childrens.removeClass('smooth-down').addClass('smooth-up');
-                  }
-                },
-    };
+      $current_popup_block.fadeToggle(fade_options);
+    });
+  }
 
-    $current_popup_block.fadeToggle(fade_options);
-  });
+  makePopupCard();
 });
